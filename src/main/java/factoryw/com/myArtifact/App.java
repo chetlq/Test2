c17128df3f4ee09ee53383d9c2d76a6a53e7ff69
chetlq <chetl@mail.ru>
2016-06-15 01:44:49 +0300

    qwe
    qwe

---
+++ src/main/java/factoryw/com/myArtifact/App.java
@@ -0,0 +1,13 @@
+package factoryw.com.myArtifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
